5adcec2d62a1bb53298f37626600325ea9efe6a0
m003357c <m003357c@student.staffs.ac.uk>
2017-04-07 17:55:01 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,19 +9,39 @@
         map: map
     });
 }*/
-
 window.addEventListener('load', displayMap );
-function displayMap() {
+let options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+const output = document.getElementById("myLocation");
+function displayMap(pos) {
+	
+	let latitude  = pos.coords.latitude;
+    let longitude = pos.coords.longitude;
+	let accuracy = pos.coords.accuracy;
+
     if (document.getElementById( 'cineMap' ) === null) {
         return false; 
     } 
-    var stoke = {lat: 53.010274, lng: -2.178498};
-    var map = new google.maps.Map(document.getElementById('cineMap'), {
-        zoom: 13,
-        center: stoke
-    });
+    //latitude and longitude for Mellor building 
+    var userLoc = new google.maps.LatLng(${latitude}, ${longitude}); 
+    //set up mapOptions (Zoom = 0 - zoomed out)
+    var mapOptions = {
+        zoom: 12, 
+        center: userLoc 
+    };     
+    //create map using mapOptions 
+    var map = new google.maps.Map(document.getElementById('cineMap'), mapOptions);
+    
     var marker = new google.maps.Marker({
-        position: stoke,
+        position: userLoc,
         map: map
     });
-}
+
+};
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+};
+navigator.geolocation.getCurrentPosition(displayMap, error, options);
