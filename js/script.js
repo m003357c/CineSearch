decc824d55e3d45494f637d07184c0bf66e17c16
m003357c <m003357c@student.staffs.ac.uk>
2017-04-07 18:13:41 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,11 +11,12 @@ function success(pos) {
 	let longitude = pos.coords.longitude;
 	
 	//latitude and longitude for Mellor building  
-	var stoke = new google.maps.LatLng(latitude, longitude); 
+	var userLoc = new google.maps.LatLng(latitude, longitude); 
 	//set up mapOptions (Zoom = 0 - zoomed out)  
 	var mapOptions = {   
 		zoom: 12,  
-		center: stoke  
+		center: userLoc,
+		disableDefaultUI: false
 	};    
 	//create map using mapOptions 
 	var map = new google.maps.Map(document.getElementById('cineMap' ), mapOptions);
@@ -25,6 +26,7 @@ function success(pos) {
 		position: stoke,
 		title: "stoke position",
 		map: map
+		
 	});
 };
 function error(err) {
