e233a2563a39a053aa2a07726014e61040818ed5
m003357c <m003357c@student.staffs.ac.uk>
2017-04-07 17:44:07 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-function initMap() {
+/*function initMap() {
+    var stoke = {lat: 53.010274, lng: -2.178498};
+    var map = new google.maps.Map(document.getElementById('cineMap'), {
+        zoom: 13,
+        center: stoke
+    });
+    var marker = new google.maps.Marker({
+        position: stoke,
+        map: map
+    });
+}*/
+
+window.addEventListener('load', displayMap );
+function displayMap() {
+    if (document.getElementById( 'cineMap' ) === null) {
+        return false; 
+    } 
     var stoke = {lat: 53.010274, lng: -2.178498};
     var map = new google.maps.Map(document.getElementById('cineMap'), {
         zoom: 13,
