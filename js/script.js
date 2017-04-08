c264fd41e965406f49f280b324a84e0b5c387540
m003357c <m003357c@student.staffs.ac.uk>
2017-04-08 14:32:53 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -64,7 +64,7 @@ if(filmContainer){
         });
 }*/
 $(document).ready(function(){
-	$('.hamburger').click(function(){
+	$('.hamburger').parent().click(function(){
 		$("nav").addClass("is-showing");
 		
 	});	
