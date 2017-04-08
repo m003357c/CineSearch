4d41be503b3a927d0d4f1f1c228df542b288106d
m003357c <m003357c@student.staffs.ac.uk>
2017-04-08 13:14:16 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+		console.log('Service worker registered successfully');
+	}).catch(function(err) {
+		console.log('Service worker registration failed: ', err);
+	});
+}
+
 let options = {
   enableHighAccuracy: true,
   timeout: 5000,
