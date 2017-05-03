253e6e2758b98093232628bd1213a3bf5ab508de
m003357c <m003357c@student.staffs.ac.uk>
2017-05-03 14:42:49 +0100

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -4,12 +4,12 @@ var CACHED_URLS = [
   // HTML
     BASEPATH + 'index.html',
   // Stylesheets and fonts    
-    BASEPATH +  'css/styles.css',    
+    BASEPATH +  'css/style-min.css',    
   // JavaScript
     BASEPATH +  'assets/icons/manifest.json',
     BASEPATH +  'js/films.json',
     BASEPATH +  'js/cinemas.json',
-    BASEPATH +  'js/script.js',
+    BASEPATH +  'js/script-min.js',
     BASEPATH +  'js/offline-map.js',    
   // Images
     BASEPATH + 'assets/images/fantastic-thumb.jpg',
