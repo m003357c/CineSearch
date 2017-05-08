5c550d274ea9530365462b191de05b2a1437eeff
m003357c <m003357c@student.staffs.ac.uk>
2017-05-08 20:14:16 +0100

    Update offline-map.js

--- js/offline-map.js
+++ js/offline-map.js
@@ -5,84 +5,42 @@ var offlineFilm = JSON.parse(localStorage.getItem('lastVisited'));
 console.log(offlineFilm);
 
 const offlineBoxOffice = document.getElementById('boxOffice');
-if(offlineBoxOffice){
-    fetch("js/films.json")
-        .then(response => {
-            return response.json();
-        }).then(films => {
-            const filmOutput = films.map(film => {
-                return `<div class="film-list-land">
-                          <div class="film-out">
-                            <div class="film-thumb">
-                                <img src="${film[0].picture}" alt="${film[0].name} Movie Poster"/>
-                            </div>
-                            <div class="film-times">
-                                <h5>${film[0].name}</h5>
-                                <p>
-                                    <a href="#" class="date-controls active">Today</a>
-                                    <a href="#" class="date-controls">17/04/17</a>
-                                    <a href="#" class="date-controls">18/04/17</a>
-                                </p>
-                                <table>
-                                  <tr>
-                                    <th>3D</th>
-                                    <td>${film[0].times.td[0]}</td>
-                                    <td>${film[0].times.td[1]}</td>
-                                    <td>${film[0].times.td[2]}</td>
-                                    <td>${film[0].times.td[3]}</td>
-                                    <td>${film[0].times.td[4]}</td>
-                                  </tr>
-                                  <tr>
-                                    <th>Standard</th>
-                                    <td>${film[0].times.standard[0]}</td>
-                                    <td>${film[0].times.standard[1]}</td>
-                                    <td>${film[0].times.standard[2]}</td>
-                                    <td>${film[0].times.standard[3]}</td>
-                                    <td>${film[0].times.standard[4]}</td>
-                                  </tr>
-                                </table>
-                                </div>
-                              </div>
-                            </div>
-                            <div class="film-list-port">
-                              <div class="film-out">
-                                <div class="film-thumb">
-                                <img src="${film[0].picture}" alt="${film[0].name} Movie Poster"/>
-                                </div>
-                                <div class="film-times">
-                                <h5>${film[0].name}</h5>
-                                <p>
-                                    <a href="#" class="btn date-change">&lt;</a>
-                                    Today
-                                    <a href="#" class="btn date-change">&gt;</a>
-                                </p>
-                                <ul>
-                                  <li class="dropdown">
-                                    <a href="#">3D</a>
-                                    <ul class="dropdown-menu">
-                                      <li>${film[0].times.td[0]}</li>
-                                      <li>${film[0].times.td[1]}</li>
-                                      <li>${film[0].times.td[2]}</li>
-                                    </ul>
-                                  </li>
-                                  <li class="dropdown">
-                                    <a href="#">Standard</a>
-                                    <ul class="dropdown-menu">
-                                      <li>${film[0].times.standard[1]}</li>
-                                      <li>${film[0].times.standard[2]}</li>
-                                      <li>${film[0].times.standard[3]}</li>
-                                      <li>${film[0].times.standard[4]}</li>
-                                    </ul>
-                                  </li>
-                                </ul>
-                                </div>
-                              </div>
-                            </div>`;
-            }).join("\n");            
-            offlineBoxOffice.innerHTML = filmOutput;
-        });
-}
+const filmOutput =  "<div class='film-list-port'>" +
+                  "<div class='film-out'>" +
+                    "<div class='film-thumb'>" +
+                    "<img src='${film[0].picture}' alt='"+ offlineFilm + " Movie Poster'/>" +
+                    "</div>" +
+                    "<div class='film-times'>" +
+                    "<h5>"+ offlineFilm + "</h5>" +
+                    "<p>" +
+                        "<a href='#' class='btn date-change'>&lt;</a>" +
+                       " Today" +
+                       " <a href='#' class='btn date-change'>&gt;</a>" +
+                    "</p>" +
+                    "<ul>" +
+                      "<li class='dropdown'>" +
+                       " <a href='#'>3D</a>" +
+                        "<ul class='dropdown-menu'>" +
+                          "<li>${film[0].times.td[0]}</li>" +
+                          "<li>${film[0].times.td[1]}</li>" +
+                          "<li>${film[0].times.td[2]}</li>" +
+                       " </ul>" +
+                     " </li>" +
+                      "<li class='dropdown'>" +
+                       " <a href='#'>Standard</a>" +
+                        "<ul class='dropdown-menu'>" +
+                         " <li>${film[0].times.standard[1]}</li>" +
+                         " <li>${film[0].times.standard[2]}</li>" +
+                         " <li>${film[0].times.standard[3]}</li>" +
+                         " <li>${film[0].times.standard[4]}</li>" +
+                        "</ul>" +
+                     " </li>" +
+                   " </ul>" +
+                   " </div>" +
+                  "</div>" +
+                "</div>";
 
-
+offlineBoxOffice.innerHTML = filmOutput;
+   
 var offlineSearch = '<h2>You are offline</h2><p>The films and times below are from your previous search</p>';
 document.getElementById('searchBox').innerHTML = offlineSearch;
