89292a03b817747ab37e1a2a0ff7459bef9c7449
m003357c <m003357c@student.staffs.ac.uk>
2017-05-08 21:07:27 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -193,7 +193,7 @@ $(document).ready(function(){
 										  '</div>' +
 										  '<div class="film-times">' +
 											'<h5>' + film.name + '</h5>' +
-											'<p><a href="#" class="btn date-change">&lt;</a>Today<a href="#" class="btn date-change">&gt;</a></p>' +
+											'<p>Today</p>' +
 											'<ul>' +
 												'<li class="dropdown">' +
 													'<a href="#">3D</a>' +
