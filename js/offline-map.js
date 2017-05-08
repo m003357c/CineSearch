b0c9dd0b57c690f8e49e55b59e38748c2feedce3
m003357c <m003357c@student.staffs.ac.uk>
2017-05-08 20:02:40 +0100

    Update offline-map.js

--- js/offline-map.js
+++ js/offline-map.js
@@ -13,10 +13,10 @@ if(offlineBoxOffice){
                 return `<div class="film-list-land">
                           <div class="film-out">
                             <div class="film-thumb">
-                                <img src="${film.picture}" alt="${film.name} Movie Poster"/>
+                                <img src="${film[0].picture}" alt="${film[0].name} Movie Poster"/>
                             </div>
                             <div class="film-times">
-                                <h5>${film.name}</h5>
+                                <h5>${film[0].name}</h5>
                                 <p>
                                     <a href="#" class="date-controls active">Today</a>
                                     <a href="#" class="date-controls">17/04/17</a>
@@ -25,19 +25,19 @@ if(offlineBoxOffice){
                                 <table>
                                   <tr>
                                     <th>3D</th>
-                                    <td>${film.times.td[0]}</td>
-                                    <td>${film.times.td[1]}</td>
-                                    <td>${film.times.td[2]}</td>
-                                    <td>${film.times.td[3]}</td>
-                                    <td>${film.times.td[4]}</td>
+                                    <td>${film[0].times.td[0]}</td>
+                                    <td>${film[0].times.td[1]}</td>
+                                    <td>${film[0].times.td[2]}</td>
+                                    <td>${film[0].times.td[3]}</td>
+                                    <td>${film[0].times.td[4]}</td>
                                   </tr>
                                   <tr>
                                     <th>Standard</th>
-                                    <td>${film.times.standard[0]}</td>
-                                    <td>${film.times.standard[1]}</td>
-                                    <td>${film.times.standard[2]}</td>
-                                    <td>${film.times.standard[3]}</td>
-                                    <td>${film.times.standard[4]}</td>
+                                    <td>${film[0].times.standard[0]}</td>
+                                    <td>${film[0].times.standard[1]}</td>
+                                    <td>${film[0].times.standard[2]}</td>
+                                    <td>${film[0].times.standard[3]}</td>
+                                    <td>${film[0].times.standard[4]}</td>
                                   </tr>
                                 </table>
                                 </div>
@@ -46,10 +46,10 @@ if(offlineBoxOffice){
                             <div class="film-list-port">
                               <div class="film-out">
                                 <div class="film-thumb">
-                                <img src="${film.picture}" alt="${film.name} Movie Poster"/>
+                                <img src="${film[0].picture}" alt="${film[0].name} Movie Poster"/>
                                 </div>
                                 <div class="film-times">
-                                <h5>${film.name}</h5>
+                                <h5>${film[0].name}</h5>
                                 <p>
                                     <a href="#" class="btn date-change">&lt;</a>
                                     Today
@@ -59,24 +59,19 @@ if(offlineBoxOffice){
                                   <li class="dropdown">
                                     <a href="#">3D</a>
                                     <ul class="dropdown-menu">
-                                      <li>${film.times.td[0]}</li>
-                                      <li>${film.times.td[1]}</li>
-                                      <li>${film.times.td[2]}</li>
+                                      <li>${film[0].times.td[0]}</li>
+                                      <li>${film[0].times.td[1]}</li>
+                                      <li>${film[0].times.td[2]}</li>
                                     </ul>
                                   </li>
                                   <li class="dropdown">
                                     <a href="#">Standard</a>
-                                    <ul class="dropdown-menu">`
-                                        /*film.times.standard.forEach(function(item, i){
-                                            var standardFilmTimes = `<li>${film.times.standard[i]}</li>`;
-                                            
-                                        });*/
-                                      
-                                      //<li>${film.times.standard[1]}</li>
-                                      //<li>${film.times.standard[2]}</li>
-                                      //<li>${film.times.standard[3]}</li>
-                                      //<li>${film.times.standard[4]}</li>
-                                    `</ul>
+                                    <ul class="dropdown-menu">
+                                      <li>${film[0].times.standard[1]}</li>
+                                      <li>${film[0].times.standard[2]}</li>
+                                      <li>${film[0].times.standard[3]}</li>
+                                      <li>${film[0].times.standard[4]}</li>
+                                    </ul>
                                   </li>
                                 </ul>
                                 </div>
@@ -90,8 +85,3 @@ if(offlineBoxOffice){
 
 var offlineSearch = '<h2>You are offline</h2><p>The films and times below are from your previous search</p>';
 document.getElementById('searchBox').innerHTML = offlineSearch;
-
-/*$(document).ready(function(){
-    $(".film-list-land td:contains('undefined')").empty();
-    $(".film-list-port .dropdown-menu li:contains('undefined')").remove();
-});*/
