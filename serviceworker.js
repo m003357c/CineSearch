e15e63c0b580e28e440fe353e10b93c25eac5d8f
m003357c <m003357c@student.staffs.ac.uk>
2017-05-08 20:07:02 +0100

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -10,6 +10,7 @@ var CACHED_URLS = [
     BASEPATH +  'js/films.json',
     BASEPATH +  'js/cinemas.json',
     BASEPATH +  'js/scripts-min.js',
+    BASEPATH +  'js/scripts.js',
     BASEPATH +  'js/offline-map.js',    
   // Images
     BASEPATH + 'assets/images/fantastic-thumb.jpg',
